d38803e90b12b60250c073ba6323cdb79ea7ed47
davidecaputo <daca9621@gmail.com>
2023-03-23 03:26:37 +0100

    Create array

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/*
+    Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+    Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+*/
+
+const team = [
+    {
+        name: 'Wayne',
+        surname: 'Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela',
+        surname: 'Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter',
+        surname: 'Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela',
+        surname: 'Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott',
+        surname: 'Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara',
+        surname: 'Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
